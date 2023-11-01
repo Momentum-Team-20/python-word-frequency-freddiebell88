b39c1c0454b549cf44ea14ac983343543c1344a3
freddiebell88 <frederikcwbell@gmail.com>
2023-11-01 13:58:35 -0400

    removed punctuation and made lower case

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,20 +7,34 @@ STOP_WORDS = [
 ]
 
 
+def remove_punctuation(str):
+    for character in str:
+        if character in string.punctuation:
+            str = str.replace(character, "")
+    return str
+
+
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    word_dict = dict()
+    word_count = {}
     with open(file) as reader:
         text = reader.read()
-        words = text.split()
-        # print(words)
+        cleaned_text = remove_punctuation(text)
+        words = cleaned_text.split()
+        # print(string.punctuation)
+        
         for word in words:
-            # print(word)
-            word_dict[word] = word_dict.get(word, 0) + 1
+            word = word.lower()
+            # if word in word_dict:
+            #     word_dict[word] = word_dict[word] + 1
+            # else:
+            #     word_dict[word] = 1
+            word_count[word] = word_count.get(word, 0) + 1
     # print(word_dict)
 
-    for key, value in word_dict.items():
+    for key, value in word_count.items():
         print(key, value)
+
 
 if __name__ == "__main__":
     import argparse
